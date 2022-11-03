d60a7e5794787acb12e57de11fee93622324b60c
Sito8943 <sito8943@gmail.com>
2022-11-02 20:50:39 -0400

    added level to heros

--- models/Heros.js
+++ models/Heros.js
@@ -12,9 +12,45 @@ class Hero extends Unit {
    * @param {number} speed
    * @param {number} creationTime
    * @param {string[]} req
+   * @param {number} level
    */
-  constructor(id, name, price, life, speed, creationTime, req = []) {
+  constructor(id, name, price, life, speed, creationTime, req = [], level = 1) {
     super(id, name, price, life, speed, creationTime, req);
+    this.level = level;
+  }
+
+  createUnit(
+    options = {
+      id: "",
+      name: "",
+      price: {},
+      life: 0,
+      speed: 0,
+      creationTime: 0,
+      req: [],
+      level: 1,
+    }
+  ) {
+    const { id, name, price, life, speed, creationTime, req, level } = options;
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.creationTime = creationTime;
+    this.currentPrice = { ...price };
+    this.life = life;
+    this.currentLife = life;
+    this.speed = speed;
+    this.currentSpeed = speed;
+    this.req = req;
+    this.level = level;
+  }
+
+  get Level() {
+    return this.level;
+  }
+
+  set Level(newLevel) {
+    this.level = newLevel;
   }
 }
 
